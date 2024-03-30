8c9807ff7103147fc2e6fb55a11bef6aa7ad8a98
Omkar Date <omkardate29@gmail.com>
2024-03-30 21:45:54 +0530

    connection snippet for next.js application

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,24 @@ if (!process.env.DB_URL) {
   throw new Error("🥲 Database credentials missing!");
 }
 
+type DBType = NodePgDatabase<typeof schema>;
+
+declare global {
+  var db: DBType | undefined;
+}
+
+let db: DBType;
+
 const { Pool } = pg; // why the fuck i have to do this
 const pool = new Pool({ connectionString: process.env.DB_URL });
 
-export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(pool, { schema });
+} else {
+  if (!global.db) {
+    global.db = drizzle(pool, { schema });
+  }
+  db = global.db;
+}
+
+export { db };
